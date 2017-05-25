97a9c36b30a01cde97e806886af185e07fdabc48
fujianchao <fujianchao@hotmail.com>
2017-05-25 14:07:58 +0800

    test

--- test/testapp/testuser.py
+++ test/testapp/testuser.py
@@ -4,6 +4,7 @@ from app.customer.models.user import *
 class TestUserDeultImg:
     def test_get_all_default_img(self):
         pictures = UserDefaultImg.get_all_defaults_img()
+        print "=="
         for picture in pictures:
             print picture.picture_url
             assert picture.picture_url in UserDefaultImg.Picture_info
