32505d687757002dec654c6de3dfb85b6f5c5c53
biwei <biweibiren@163.com>
2017-06-17 10:10:57 +0800

    fix 新人驾到有重复 千里眼有自己

--- background/cron/ranks.py
+++ background/cron/ranks.py
@@ -38,7 +38,8 @@ def new_anchors():
         user = User.objects.filter(id=verify.user_id).first()
         user_beat = UserHeartBeat.objects.filter(user=user, last_report_time__gte=pre_time)
         if user_beat:
-            user_list.append(user)
+            if user not in user_list:
+                user_list.append(user)
 
     NewAnchorRank.drop_collection()
 
