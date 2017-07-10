a95439a69e3887f6d995f6d2a929fbd082489f94
zhangzhe <727236692@qq.com>
2017-07-10 14:25:34 +0800

    推送接口修改

--- api/handler/getui_controller.py
+++ api/handler/getui_controller.py
@@ -10,6 +10,7 @@ from app.customer.models.getuiusers import GeTuiUsers
 class AddGeTui(BaseHandler):
     @api_define("add getui record", "/getui/add_record",
                 [
+                    Param("user_id",True, str, "", "", u"用户短id"),
                     Param("dev_no", True, str, "", "", u"客户端设备号"),
                     Param("cid", True, str, "", "", u"个推clientid"),
                     Param("province", True, str, "", "", u"省"),
@@ -30,73 +31,36 @@ class AddGeTui(BaseHandler):
         address = self.arg("address", "")
         os_version = self.arg("os_version", "")
         platfrom = self.arg("platfrom", "")
-        gtuser = GeTuiUsers()
-        gtuser.dev_no = dev_no
-        gtuser.cid = cid
-        gtuser.appname = app_name
-        gtuser.province = province
-        gtuser.city = city
-        gtuser.districts = districts
-        gtuser.address = address
-        gtuser.os_version = os_version
-        gtuser.platfrom = platfrom
-        gtuser.create_time = datetime.datetime.now()
-        try:
-            gtuser.save()
+        user_id = self.arg("user_id", "")
+        record = GeTuiUsers.objects.filter(dev_no = dev_no).first()
+        if record:
+            if user_id != "":
+                try:
+                    record.user_id = int(user_id)
+                    record.save()
+                except Exception,e:
+                    logging.error("create Getui error:{0}".format(e))
+                return self.write({"status": "fail","error": u"上报设备信息失败"})
             return self.write({"status": "success"})
-        except Exception,e:
-            logging.error("create Getui error:{0}".format(e))
-            return self.write({"status": "fail","error": u"上报设备信息失败"})
+        else :
+                gtuser = GeTuiUsers()
+                gtuser.dev_no = dev_no
+                gtuser.cid = cid
+                gtuser.appname = app_name
+                gtuser.province = province
+                gtuser.city = city
+                gtuser.districts = districts
+                gtuser.address = address
+                gtuser.os_version = os_version
+                gtuser.platfrom = platfrom
+                gtuser.create_time = datetime.datetime.now()
+                try:
+                    gtuser.save()
+                    return self.write({"status": "success"})
+                except Exception,e:
+                    logging.error("create Getui error:{0}".format(e))
+                    return self.write({"status": "fail","error": u"上报设备信息失败"})
 
-@handler_define
-class UpdateGeTui(BaseHandler):
-    @api_define("update getui record", "/getui/update_record",
-                [
-                    Param("dev_no", True, str, "", "", u"设备号"),
-                    Param("user_id", True, str, "", "", u"用户短id"),
-                ], description=u"绑定用户设备")
-    def get(self):
-        resultmap ={}
-        dev_no = self.arg("dev_no", "")
-        user_id = self.arg("user_id", "")
-        if dev_no != "" and user_id != "":
-            record = GeTuiUsers.objects.filter(dev_no = dev_no).first()
-            if record:
-                record.user_id = int(user_id)
-                record.save()
-                resultmap["status"] = "success"
-            else:
-                resultmap["status"] = "fail"
-                resultmap["error"] = u"无该设备号的记录"
-        else:
-            resultmap["status"] = "fail"
-            resultmap["error"] = u"设备号和用户id不能为空"
-        return self.write(resultmap)
-
-@handler_define
-class UnBindGeTui(BaseHandler):
-    @api_define("unbind getui record", "/getui/unbind_user",
-                [
-                    Param("dev_no", True, str, "", "", u"设备号"),
-                    Param("user_id", True, str, "", "", u"用户短id"),
-                ], description=u"解绑用户设备")
-    def get(self):
-        resultmap ={}
-        dev_no = self.arg("dev_no", "")
-        user_id = self.arg("user_id", "")
-        if dev_no != "" and user_id != "":
-            record = GeTuiUsers.objects.filter(dev_no = dev_no,user_id=int(user_id)).first()
-            if record:
-                record.user_id = None
-                record.save()
-                resultmap["status"] = "success"
-            else:
-                resultmap["status"] = "fail"
-                resultmap["error"] = u"无该设备号及userid的记录"
-        else:
-            resultmap["status"] = "fail"
-            resultmap["error"] = u"设备号和用户id不能为空"
-        return self.write(resultmap)
 
 
 
