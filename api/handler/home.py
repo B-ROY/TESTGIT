b6e815e3c7da7928f3f65a94ab4a639eb10303a8
biwei <biweibiren@163.com>
2017-08-14 18:50:04 +0800

    新人驾到改成60分钟在线

--- api/handler/home.py
+++ api/handler/home.py
@@ -149,7 +149,7 @@ class Get_Index_Column(BaseHandler):
                 # 是否在线 查看心跳
                 import time
                 time = int(time.time())
-                pre_time = time - 120
+                pre_time = time - 3600
                 user_beat = UserHeartBeat.objects.filter(user=user, last_report_time__gte=pre_time).first()
                 if user_beat:
                     is_online = 1
